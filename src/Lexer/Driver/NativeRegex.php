068ea20e5ec2bdab27aa022b153b38d5b4aeadb5
Kirill Nesmeyanov <nesk@xakep.ru>
2019-01-24 22:33:38 +0000

    Apply fixes from StyleCI

--- src/Lexer/Driver/NativeRegex.php
+++ src/Lexer/Driver/NativeRegex.php
@@ -30,7 +30,7 @@ class NativeRegex extends SimpleLexer
      */
     public function __construct(array $tokens = [], array $skip = [])
     {
-        $this->tokens = $tokens;
+        $this->tokens  = $tokens;
         $this->skipped = $skip;
     }
 
@@ -43,7 +43,7 @@ class NativeRegex extends SimpleLexer
     protected function exec(Readable $file): \Traversable
     {
         $offset = 0;
-        $regex = new RegexNamedGroupsIterator($this->getPattern(), $file->getContents());
+        $regex  = new RegexNamedGroupsIterator($this->getPattern(), $file->getContents());
 
         $iterator = $regex->getIterator();
 
